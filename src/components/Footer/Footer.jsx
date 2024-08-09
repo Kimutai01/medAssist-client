0c24cff88ea68c2a93ab9e2bbcbd0e9e9a86e55d
Kimutai01 <kiprotichkimutai01@gmail.com>
2024-08-09 13:43:57 +0300

    year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => (
         <span class="ml-3 text-xl">MedAssist</span>
       </a>
       <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-        © 2023 MedAssist
+        © 2024 MedAssist
       </p>
       <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
         <a class="text-gray-500">
